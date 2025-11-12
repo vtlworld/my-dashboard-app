e619e9bee5e468b0f09ed1eddc11f4e524b2fecf
vtlworld <vtlworld.com@gmail.com>
2025-11-12 19:21:15 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+// A very simple service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // We'll add caching logic here later
+  event.respondWith(fetch(event.request));
+});
